be53a566ab00f33a2bdb31aba5e68ea9a881b757
喵了个咪 <841658979@qq.com>
2021-09-03 22:22:13 +0800

    添加项目文件。

---
+++ HelloWorld.cs
@@ -0,0 +1,24 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstRevitProject
+{
+    [Transaction(TransactionMode.Manual)]
+    public class HelloWorld : IExternalCommand
+    {
+        
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+
+            TaskDialog.Show("HelloWorld","My first revit project!!!");
+            return Result.Succeeded;
+            //throw new NotImplementedException();
+        }
+    }
+}
